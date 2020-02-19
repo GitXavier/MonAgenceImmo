cc7910a79f1c4fadbc8b8c42cbf55ce85ae7bd54
Xavier <x.crochet@gmail.com>
2020-02-19 16:36:47 +0100

    Configuration webPack + Modification path asset dans html

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,23 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+import './jquery-2.1.0.min';
+import '../bootstrap/js/bootstrap.min';
+import './smoothscroll';
+import './owl.carousel.min';
+import './bootstrap-select.min';
+import './jquery.validate.min';
+import './jquery.placeholder';
+import './icheck.min';
+import './jquery.vanillabox-0.1.5.min';
+import './retina-1.1.0.min';
+import './jshashtable-2.1_src';
+import './jquery.numberformatter-1.2.3';
+import './tmpl';
+import './jquery.dependClass-0.1';
+import './draggable-0.1';
+import './jquery.slider';
+import './custom';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
