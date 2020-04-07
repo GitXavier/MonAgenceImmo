a4375a5af6c24b8fdacbba58ea7384de71578651
Yannick6633 <yanlechaka@sfr.fr>
2020-04-07 16:42:52 +0200

    Envoi email

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
